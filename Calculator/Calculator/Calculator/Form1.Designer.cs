facea709369832dc15bfbd07598329f8c00e3a0b
SuphonSaejia <53931544+SuphonSaejia@users.noreply.github.com>
2020-09-05 22:05:59 +0700

    Add files via upload

---
+++ Calculator/Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCal = new System.Windows.Forms.Button();
+            this.txtInfix = new System.Windows.Forms.TextBox();
+            this.txtPostfix = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtTmpInfix = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCal
+            // 
+            this.btnCal.Location = new System.Drawing.Point(117, 167);
+            this.btnCal.Name = "btnCal";
+            this.btnCal.Size = new System.Drawing.Size(132, 37);
+            this.btnCal.TabIndex = 0;
+            this.btnCal.Text = "Calculate";
+            this.btnCal.UseVisualStyleBackColor = true;
+            this.btnCal.Click += new System.EventHandler(this.btnCal_Click);
+            // 
+            // txtInfix
+            // 
+            this.txtInfix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInfix.Location = new System.Drawing.Point(117, 40);
+            this.txtInfix.Name = "txtInfix";
+            this.txtInfix.Size = new System.Drawing.Size(247, 26);
+            this.txtInfix.TabIndex = 1;
+            // 
+            // txtPostfix
+            // 
+            this.txtPostfix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPostfix.Location = new System.Drawing.Point(117, 103);
+            this.txtPostfix.Name = "txtPostfix";
+            this.txtPostfix.Size = new System.Drawing.Size(247, 26);
+            this.txtPostfix.TabIndex = 2;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(117, 135);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(247, 26);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Infix";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Postfix";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Output";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "TmpInfix";
+            // 
+            // txtTmpInfix
+            // 
+            this.txtTmpInfix.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTmpInfix.Location = new System.Drawing.Point(117, 72);
+            this.txtTmpInfix.Name = "txtTmpInfix";
+            this.txtTmpInfix.Size = new System.Drawing.Size(247, 26);
+            this.txtTmpInfix.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 290);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTmpInfix);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtPostfix);
+            this.Controls.Add(this.txtInfix);
+            this.Controls.Add(this.btnCal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCal;
+        private System.Windows.Forms.TextBox txtInfix;
+        private System.Windows.Forms.TextBox txtPostfix;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtTmpInfix;
+    }
+}
+
---
+++ Calculator/Calculator/Calculator/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCal_Click(object sender, EventArgs e)
+        {
+            char[] InfixArr = txtInfix.Text.ToCharArray();
+            txtTmpInfix.Text = parseInfix(InfixArr);
+            txtPostfix.Text = convertToPostfix(txtTmpInfix.Text);
+            txtOutput.Text = calculatePostfix(txtPostfix.Text);
+        }
+
+        private string parseInfix(char[] input)  //function สำหรับการใส่ช่องว่างในแยกแต่ละ Token
+        {
+            string ret="";
+            char tmp;
+            for (int i = 0; i < input.Length ; i++)
+            { int j,k;
+                tmp = input[i];
+                if (tmp == '.')
+                {
+                    ret += tmp;
+                }
+                else if (int.TryParse(tmp.ToString(), out j))  //ตรวจสอบว่า Token เป็นตัวเลขหรือไม่ ถ้าเป็นตัวเลขจะนำไปต่อกันตามจำนวนหลัก
+                {
+                    ret += tmp.ToString();
+                    if(i<input.Length-1)   //ตรวจสอบว่าเป็น Token ตัวสุดท้ายหรือไม่
+                     if(!int.TryParse(input[i+1].ToString(),out k) && input[i+1]!='.')  // ถ้าตัวถัดไปไม่ใช่ Token ตัวสุดท้ายและไม่ได้เป็นตัวเลข คือเป็น ( + - * / ) ให้เพิ่ม space เข้าไป 
+                    {
+                        ret += " ";
+                    }
+                }
+                else
+                {
+                    ret += tmp.ToString()+ " "; // กรณี Token ไม่ใช่ตัวเลข ให้นำมาใส่ space ต่อท้าย
+                }
+            
+            }
+            return ret;
+        }
+
+
+        private string convertToPostfix(string infix)
+        {
+            string ret = "";
+            infix = infix.Trim();
+            string[] infixArr = infix.Split(' ');
+            Stack<string> s = new Stack<string>();
+            foreach (string token in infixArr)
+            {
+                switch (token)
+                {   
+                    case "(":
+                        s.Push(token);
+                        break;
+                    case "+":
+                        if (s.Count > 0)
+                        while("+-*/".Contains(s.Peek()))
+                        {
+                            ret += s.Pop() + " ";
+                        }
+                        s.Push(token);
+                        break;
+                    case "-":
+                        if(s.Count>0)
+                        while ("+-*/".Contains(s.Peek()))
+                        {
+                            ret += s.Pop() + " ";
+                        }
+                        s.Push(token);
+                        break;
+                    case "*":
+                        if (s.Count > 0)
+                        while("*/".Contains(s.Peek()))
+                        {
+                            ret += s.Pop() + " ";
+                        }
+                        s.Push(token);
+                        break;
+                    case "/":
+                        if (s.Count > 0)
+                       while("*/".Contains(s.Peek()))
+                        {
+                            ret += s.Pop() + " ";
+                        }
+                       s.Push(token);
+                        break;
+                    case ")":
+                        do
+                        {
+                            ret += s.Pop() + " ";
+                        } while (s.Peek() != "(");
+
+                        if (s.Count > 0)
+                        if (s.Peek() == "(") s.Pop();
+                             break;
+                    default:
+                        ret +=token + " ";
+                        break;
+                }
+               
+            }
+            while(s.Count > 0) ret += s.Pop() +" ";
+        
+        return ret;
+        }
+
+        private string calculatePostfix(string postfix)
+        {
+            string ret="";
+            double a, b;
+            Stack<double> s = new Stack<double>();
+            postfix = postfix.Trim();
+            string[] postfixArr = postfix.Split(' ');
+            foreach (string token in postfixArr)
+            {
+                if ("+-*/".Contains(token))
+                {
+                    b = s.Pop();
+                    a = s.Pop();
+                    switch (token)
+                    {
+                        case "+": s.Push(a + b); break;
+                        case "-": s.Push(a - b); break;
+                        case "*": s.Push(a * b); break;
+                        case "/": s.Push(a / b); break;
+                    }
+
+                }
+                else
+                {
+                    s.Push(double.Parse(token));
+                }
+            }
+            ret = s.Pop().ToString();
+            return ret;
+        }
+
+       
+    }
+}
